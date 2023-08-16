25c84756662f0fca4198eee0eb2eb28d6377a56d
Michaux Kelley <michauxkelley@gmail.com>
2023-08-15 20:01:08 -0400

    Fix invalid p wrapping a div causing error

--- app/restaurant/[slug]/components/Rating.tsx
+++ app/restaurant/[slug]/components/Rating.tsx
@@ -6,9 +6,9 @@ export default function Rating({ reviews }: { reviews: Review[] }) {
   return (
     <div className="flex items-end">
       <div className="ratings mt-2 flex items-center">
-        <p>
+        <div>
           <Stars reviews={reviews} />
-        </p>
+        </div>
         <p className="text-reg ml-3">
           {calculateReviewRatingAverage(reviews).toFixed(1)}
         </p>
