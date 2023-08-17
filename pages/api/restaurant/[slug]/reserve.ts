c81825d7085679f4d3de382677677cd900e89e06
Michaux Kelley <michauxkelley@gmail.com>
2023-08-17 18:12:15 -0400

    Determine the tables to book

--- pages/api/restaurant/[slug]/reserve.ts
+++ pages/api/restaurant/[slug]/reserve.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { partySize } from '../../../../data';
 import { findAvailableTables } from '../../../../services/restaurant/findAvailableTables';
 import getInvalidDataResponse from '../../../../utils/getInvalidResponse';
 
@@ -9,10 +10,11 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { slug, day, time } = req.query as {
+  const { slug, day, time, partySize } = req.query as {
     slug: string;
     day: string;
     time: string;
+    partySize: string;
   };
 
   const restaurant = await prisma.restaurant.findUnique({
@@ -20,13 +22,16 @@ export default async function handler(
     select: { tables: true, open_time: true, close_time: true },
   });
 
-  if (!restaurant) return getInvalidDataResponse(res, 'Restaurant not found');
+  if (!restaurant) {
+    return getInvalidDataResponse(res, 'Restaurant not found');
+  }
 
   if (
     new Date(`${day}T${time}`) < new Date(`${day}T${restaurant.open_time}`) ||
     new Date(`${day}T${time}`) > new Date(`${day}T${restaurant.close_time}`)
-  )
+  ) {
     return getInvalidDataResponse(res, 'Restaurant is not open at that time');
+  }
 
   const searchTimesWithTables = await findAvailableTables({
     time,
@@ -35,15 +40,61 @@ export default async function handler(
     restaurant,
   });
 
-  if (!searchTimesWithTables) return getInvalidDataResponse(res);
+  if (!searchTimesWithTables) {
+    return getInvalidDataResponse(res);
+  }
 
   const searchTimeWithTables = searchTimesWithTables.find((t) => {
     return t.date.toISOString() === new Date(`${day}T${time}`).toISOString();
   });
 
-  if (!searchTimeWithTables)
+  if (!searchTimeWithTables) {
     getInvalidDataResponse(res, 'No availability, cannot book');
+  }
 
-  return res.json({ searchTimeWithTables });
+  const tablesCount: {
+    2: number[];
+    4: number[];
+  } = {
+    2: [],
+    4: [],
+  };
+
+  searchTimeWithTables?.tables.forEach((table) => {
+    if (table.seats === 2) {
+      tablesCount[2].push(table.id);
+    } else {
+      tablesCount[4].push(table.id);
+    }
+  });
+
+  const tablesToBook: number[] = [];
+  let seatsRemaining = +partySize;
+
+  while (seatsRemaining > 0) {
+    if (seatsRemaining >= 3) {
+      if (tablesCount[4].length) {
+        tablesToBook.push(tablesCount[4][0]);
+        tablesCount[4].shift();
+        seatsRemaining = seatsRemaining - 4;
+      } else {
+        tablesToBook.push(tablesCount[2][0]);
+        tablesCount[2].shift();
+        seatsRemaining = seatsRemaining - 2;
+      }
+    } else {
+      if (tablesCount[2].length) {
+        tablesToBook.push(tablesCount[2][0]);
+        tablesCount[2].shift();
+        seatsRemaining = seatsRemaining - 2;
+      } else {
+        tablesToBook.push(tablesCount[4][0]);
+        tablesCount[4].shift();
+        seatsRemaining = seatsRemaining - 4;
+      }
+    }
+  }
+
+  return res.json({ tablesToBook });
   // http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/reserve?day=2023-02-03&time=14:00:00.000Z&partySize=4
 }
