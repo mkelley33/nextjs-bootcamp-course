5e0f88eb92aa8b4eb2be384fe9b6654a2acf5d6e
Michaux Kelley <michauxkelley@gmail.com>
2023-08-17 22:01:14 -0400

    Finish adding reservation data to the header

--- app/reserve/[slug]/components/Header.tsx
+++ app/reserve/[slug]/components/Header.tsx
@@ -1,22 +1,33 @@
+import {
+  convertToDisplayTime,
+  Time,
+} from '../../../../utils/convertToDisplayTime';
+import { format } from 'date-fns';
+
 export default function Header({
   image,
   name,
+  date,
+  partySize,
 }: {
   image: string;
   name: string;
+  date: string;
+  partySize: string;
 }) {
+  const [_, time] = date.split('T');
+
   return (
-    <div>
-      <h3 className="font-bold">You're almost done!</h3>
-      <div className="mt-5 flex">
-        <img src={image} alt="" className="w-32 h-18 rounded" />
-        <div className="ml-4">
-          <h1 className="text-3xl font-bold">{name}</h1>
-          <div className="flex mt-3">
-            <p className="mr-6">Tues, 22, 2023</p>
-            <p className="mr-6">7:30 PM</p>
-            <p className="mr-6">3 people</p>
-          </div>
+    <div className="mt-5 flex">
+      <img src={image} alt="" className="w-32 h-18 rounded" />
+      <div className="ml-4">
+        <h1 className="text-3xl font-bold">{name}</h1>
+        <div className="flex mt-3">
+          <p className="mr-6">{format(new Date(date), 'ccc, LLL d')}</p>
+          <p className="mr-6">{convertToDisplayTime(time as Time)}</p>
+          <p className="mr-6">
+            {partySize} {+partySize === 1 ? 'person' : 'people'}
+          </p>
         </div>
       </div>
     </div>
